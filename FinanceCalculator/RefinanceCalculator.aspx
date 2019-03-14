f5401a6f1205e0624d8ad7d41596c8776656dd5e
Neychok <n.kalaidjiev@gmail.com>
2019-03-14 23:05:31 +0200

    Калкулатор за рефинансиране (не работи формулата за месечна вноска /никаква идея защо/)

--- FinanceCalculator/RefinanceCalculator.aspx
+++ FinanceCalculator/RefinanceCalculator.aspx
@@ -98,7 +98,7 @@
                             <div class="col-sm-4">
                                 <div class="input-group">
                                     <span class="input-group-addon">$</span>
-                                    <asp:TextBox type="number" min="0.01" step="0.01" class="form-control input-sm" id="input7" required="required" placeholder="В %" runat="server"></asp:TextBox>
+                                    <asp:TextBox type="number" min="0" step="0.01" class="form-control input-sm" id="input7" required="required" placeholder="В %" runat="server"></asp:TextBox>
                                 </div>
                                 <asp:Label class="col-sm-6 control-label" id="error7" text="" runat="server"></asp:Label>
                             </div>
@@ -108,7 +108,7 @@
                             <div class="col-sm-4">
                                 <div class="input-group">
                                     <span class="input-group-addon">$</span>
-                                    <asp:TextBox type="number" min="1" step="1" class="form-control input-sm" id="input8" required="required" placeholder="Във валута" runat="server"></asp:TextBox>
+                                    <asp:TextBox type="number" min="0" step="1" class="form-control input-sm" id="input8" required="required" placeholder="Във валута" runat="server"></asp:TextBox>
                                 </div>
                                 <asp:Label class="col-sm-6 control-label" id="error8" text="" runat="server"></asp:Label>
                             </div>
@@ -171,15 +171,14 @@
                 tr1.appendChild(inth1);
                 var in1td1 = document.createElement("td");
                 in1td1.setAttribute("id", "in1td1");
-                in1td1.innerHTML = " %";
+                in1td1.innerHTML = <%=T_lihva%>;
                 tr1.appendChild(in1td1);
                 var in1td2 = document.createElement("td");
                 in1td2.setAttribute("id", "in1td2");
-                in1td2.innerHTML = 7 + " %";
+                in1td2.innerHTML = <%=N_lihva%>;
                 tr1.appendChild(in1td2);
                 var in1td3 = document.createElement("td");
                 in1td3.setAttribute("id", "in1td3");
-                in1td3.innerHTML = 1 + " %";
                 tr1.appendChild(in1td3);
                 tbody.appendChild(tr1);
 
@@ -191,15 +190,14 @@
                 tr2.appendChild(inth2);
                 var in2td1 = document.createElement("td");
                 in2td1.setAttribute("id", "in2td1");
-                in2td1.innerHTML = 12;
+                in2td1.innerHTML = <%=T_srok%>;
                 tr2.appendChild(in2td1);
                 var in2td2 = document.createElement("td");
                 in2td2.setAttribute("id", "in2td2");
-                in2td2.innerHTML = 7;
+                in2td2.innerHTML = <%=N_srok%>;
                 tr2.appendChild(in2td2);
                 var in2td3 = document.createElement("td");
                 in2td3.setAttribute("id", "in2td3");
-                in2td3.innerHTML = 1;
                 tr2.appendChild(in2td3);
                 tbody.appendChild(tr2);
 
@@ -211,7 +209,7 @@
                 tr3.appendChild(inth3);
                 var in3td1 = document.createElement("td");
                 in3td1.setAttribute("id", "in3td1");
-                in3td1.innerHTML = 1178.78;
+                in3td1.innerHTML = <%=predsrokTaksa%>;
                 tr3.appendChild(in3td1);
                 var in3td2 = document.createElement("td");
                 in3td2.setAttribute("id", "in3td2");
@@ -231,15 +229,15 @@
                 tr4.appendChild(inth4);
                 var in4td1 = document.createElement("td");
                 in4td1.setAttribute("id", "in4td1");
-                in4td1.innerHTML = 856.07;
+                in4td1.innerHTML = <%=T_vnoska%>;
                 tr4.appendChild(in4td1);
                 var in4td2 = document.createElement("td");
                 in4td2.setAttribute("id", "in4td2");
-                in4td2.innerHTML = 861.74;
+                in4td2.innerHTML = <%=N_vnoska%>;
                 tr4.appendChild(in4td2);
                 var in4td3 = document.createElement("td");
                 in4td3.setAttribute("id", "in4td3");
-                in4td3.innerHTML = -5.67;
+                in4td3.innerHTML = <%=spestVnoska%>;
                 tr4.appendChild(in4td3);
                 tbody.appendChild(tr4);
 
@@ -251,15 +249,15 @@
                 tr5.appendChild(inth5);
                 var in5td1 = document.createElement("td");
                 in5td1.setAttribute("id", "in5td1");
-                in5td1.innerHTML = 5992.52;
+                in5td1.innerHTML = <%=T_izplateni%>;
                 tr5.appendChild(in5td1);
                 var in5td2 = document.createElement("td");
                 in5td2.setAttribute("id", "in5td2");
-                in5td2.innerHTML = 8510.98;
+                in5td2.innerHTML = <%=N_izplateni%>;
                 tr5.appendChild(in5td2);
                 var in5td3 = document.createElement("td");
                 in5td3.setAttribute("id", "in5td3");
-                in5td3.innerHTML = -2518.46;
+                in5td3.innerHTML = <%=spestIzplateni%>;
                 tr5.appendChild(in5td3);
                 tbody.appendChild(tr5);
 
@@ -268,7 +266,7 @@
                 var intd6 = document.createElement("td");
                 intd6.setAttribute("id", "intd6");
                 intd6.setAttribute("colspan", "4");
-                intd6.innerHTML = "ИЗГОДНО";
+                intd6.innerHTML = "";
                 tr6.appendChild(intd6);
                 tbody.appendChild(tr6);
 
--- FinanceCalculator/RefinanceCalculator.aspx.cs
+++ FinanceCalculator/RefinanceCalculator.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -22,15 +23,134 @@ namespace FinanceCalculator
         //input6 - Лихва /нов кредит/
         //input7 - Първоначални такси % /нов кредит/
         //input8 - Първоначални такси Валута /нов кредит/
-        public decimal T_lihva = 0, N_lihva = 0;
+
+        public decimal predsrokTaksa = 0, T_vnoska = 0, N_vnoska = 0, T_izplateni = 0, N_izplateni = 0, spestVnoska = 0, spestIzplateni = 0;
+        public int T_srok = 0, N_srok = 0;
+        public double T_lihva = 0, N_lihva = 0;
+        public string izgodno = "";
+
         protected void CalculateResult(object sender, EventArgs e)
         {
+            int napraveniVnoski = 0;
+            decimal purvTaksi_val = 0, purvTaksi_proc = 0, Ostat_glavnica = 0, mesecLihva = 0, vnoskaGlavnica = 0, mesecVnoska = 0, creditAmount = 0;
+            bool _error = false;
 
 
+            //РАЗМЕР НА КРЕДИТА
+            creditAmount = decimal.Parse(input1.Text);
+            if (creditAmount <= 0 || creditAmount > 100000000) //ГРЕШКА - Размера на кредита е по-малък от 0 или по-голям от 100.000.000
+            {
+                _error = true;
+                error1.Text = "Размера на кредита трябва да е по-голям от 0 и по-малък от 100 000 000";
+            }
 
+            //ЛИХВА НА ТЕКУЩ КРЕДИТ
+            T_lihva = double.Parse(input2.Text, CultureInfo.InvariantCulture);
+            if (T_lihva <= 0 || T_lihva >= 100)
+            {
+                _error = true;
+                error2.Text = "Въведете коректно число за Лихва";
+            }
 
+            //СРОК НА ТЕКУЩ КРЕДИТ
+            T_srok = int.Parse(input3.Text);
+            if(T_srok <= 0 || T_srok > 960)
+            {
+                _error = true;
+                error3.Text = "Въведете коретно число за Срок на кредит (месеци)";
+            }
 
-            ScriptManager.RegisterStartupScript(this, GetType(), "showRefinanceResult", "showRefinanceResult()", true);
+            //НАПРАВЕНИ ВНОСКИ
+            napraveniVnoski = int.Parse(input4.Text);
+            if(napraveniVnoski >= T_srok || napraveniVnoski <= 0)
+            {
+                _error = true;
+                error4.Text = "Въведете коретно число за Направени вноски";
+            }
+
+            //ТАКСА ЗА ПРЕДСРОЧНО ПОГАСЯВАНЕ
+            predsrokTaksa = decimal.Parse(input5.Text, CultureInfo.InvariantCulture);
+            if (predsrokTaksa >= 100 || predsrokTaksa < 0)
+            {
+                _error = true;
+                error5.Text = "Въведете коретно число за Такса предсрочно погасяване (%)";
+            }
+
+            //ЛИХВА НА НОВ КРЕДИТ
+            N_lihva = double.Parse(input6.Text, CultureInfo.InvariantCulture);
+            if (N_lihva <= 0 || N_lihva >= 100)
+            {
+                _error = true;
+                error6.Text = "Въведете коректно число за Лихва";
+            }
+
+            //ПЪРВОНАЧАЛНИ ТАКСИ /ПРОЦЕНТ/
+            purvTaksi_proc = decimal.Parse(input7.Text, CultureInfo.InvariantCulture);
+            if (purvTaksi_proc >= 100 || purvTaksi_proc < 0)
+            {
+                _error = true;
+                error7.Text = "Въведете коретно число за Първоначални такси (%)";
+            }
+
+            //ПЪРВОНАЧАЛНИ ТАКСИ /ВАЛУТА/
+            purvTaksi_val = decimal.Parse(input8.Text);
+            if (purvTaksi_val >= creditAmount || purvTaksi_val < 0)
+            {
+                _error = true;
+                error8.Text = "Въведете коретно число за Първоначални такси (Валута)";
+            }
+
+            if (_error == false)
+            {
+                //СРОК НА НОВ КРЕДИТ
+                N_srok = T_srok - napraveniVnoski;
+
+                //ТАКСА ЗА ПРЕДСРОЧНО ПОГАСЯВАНЕ
+                Ostat_glavnica = creditAmount;
+                for (int i = 0; i <= napraveniVnoski; i++)
+                {
+                    Ostat_glavnica -= vnoskaGlavnica;
+                    mesecLihva = Ostat_glavnica * ((decimal)T_lihva / 12);
+                    mesecVnoska = MesechnaVnoska(T_lihva, T_srok - i, Ostat_glavnica);
+                    vnoskaGlavnica = mesecVnoska - mesecLihva;
+                }
+                predsrokTaksa = Ostat_glavnica * (predsrokTaksa / 100);
+
+                //МЕСЕЧНА ВНОСКА /ТЕКУЩ КРЕДИТ/
+                T_vnoska = MesechnaVnoska(T_lihva, T_srok, creditAmount);
+
+                //ОБЩО ИЗПЛАТЕНИ /ТЕКУЩ КРЕДИТ/
+                T_izplateni = T_vnoska * napraveniVnoski;
+
+                //МЕСЕЧНА ВНОСКА /НОВ КРЕДИТ/
+                N_vnoska = MesechnaVnoska(N_lihva, N_srok, creditAmount);
+
+                //ОБЩО ИЗПЛАТЕНИ /НОВ КРЕДИТ/
+                N_izplateni = N_vnoska * N_srok;
+
+                //СПЕСТЯВАНИЯ /ВНОСКА/
+                spestVnoska = T_vnoska - N_vnoska;
+
+                //СПЕСТЯВАНИЯ /ИЗПЛАТЕНИ/
+                spestIzplateni = T_izplateni - N_izplateni;
+
+                //ЗАКРЪГЛЯНЕ
+                T_vnoska = Decimal.Round(T_vnoska, 2, MidpointRounding.AwayFromZero);
+                N_vnoska = Decimal.Round(N_vnoska, 2, MidpointRounding.AwayFromZero);
+                predsrokTaksa = Decimal.Round(predsrokTaksa, 2, MidpointRounding.AwayFromZero);
+                spestIzplateni = Decimal.Round(spestIzplateni, 2, MidpointRounding.AwayFromZero);
+                spestVnoska = Decimal.Round(spestVnoska, 2, MidpointRounding.AwayFromZero);
+                N_izplateni = Decimal.Round(N_izplateni, 2, MidpointRounding.AwayFromZero);
+                T_izplateni = Decimal.Round(T_izplateni, 2, MidpointRounding.AwayFromZero);
+
+                ScriptManager.RegisterStartupScript(this, GetType(), "showRefinanceResult", "showRefinanceResult()", true);
+            }
+        }
+        protected decimal MesechnaVnoska(double interest, int months, decimal credit)
+        {
+            decimal b = (decimal)Math.Pow((interest / 12) + 1.0, -months);
+            decimal monthlyPayments = credit * ((decimal)interest / 12) / (1 - b);
+            return monthlyPayments;
         }
     }
 }
